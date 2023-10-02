33b599ae629845b3af506bd860ff9df11aac862c
Blaze Jeppesen <blazej686@yahoo.com>
2023-10-02 16:34:19 -0600

    First commit for GameNight

---
+++ app/controllers/PlayersController.js
@@ -0,0 +1,16 @@
+import { AppState } from "../AppState.js";
+import { PlayersService } from "../services/PlayersService.js";
+
+
+
+
+
+export class PlayersController {
+
+    constructor() {
+        console.console.log('player controller');
+    }
+    test() {
+        console.log('test')
+    }
+}
---
+++ app/services/PlayersService.js
@@ -0,0 +1,8 @@
+import { AppState } from "../AppState.js";
+import { Player } from "../models/PlayerStats.js";
+
+class playersService {
+
+
+}
+
